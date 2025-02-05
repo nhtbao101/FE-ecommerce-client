2e0c3595caaea824561fd61027890e16a7a07667
nhtbao101 <nhtbao101@gmail.com>
2025-02-05 15:08:54 +0700

    create service

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { store } from "./store";
-import { Provider } from "react-redux";
-import React from "react";
+import { store } from './store';
+import { Provider } from 'react-redux';
+import React from 'react';
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
